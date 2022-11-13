d3d2e5f06c6b1952ef5483ce094a6ae42c232979
Jack Pletka <jack@pletka.com>
2022-11-13 12:35:06 -0700

    Converted to ReactJS, got basic page routing and page stubs in place

---
+++ src/pages/order-processor/AcceptedOrder.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PageWrapper from "../common/PageWrapper";
+
+function AcceptedOrder() {
+    return (
+        <div>
+            <h1>AcceptedOrder</h1>
+        </div>
+    )
+}
+export default AcceptedOrder
